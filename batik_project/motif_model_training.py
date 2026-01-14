71fc5f630076177c443f58fe948be649b7efae4e
Kanaya Ozora <nayachewsudon@users.noreply.github.com>
2026-01-14 14:57:59 +0100

    Push new version of motif_model_training.py

--- batik_project/motif_model_training.py
+++ batik_project/motif_model_training.py
@@ -21,8 +21,31 @@ base_model.trainable = False
 
 model = models.Sequential([
     base_model,
-    layers.GlobalAveragePooling2D(), #compresses spatial info into a single vector
+    layers.GlobalAveragePooling2D(),
     layers.Dense(128, activation= 'relu'), #Learn combinations of features specific to motifs
-    layers.Dropout(0.5), #Drops 50% connections during training, prevent overfitting
-    layers.Dense(5, activation='softmax') #output - 5 batik motifs 
+    layers.Dropout(0.5), #Prevents overfitting
+    layers.Dense(3, activation='softmax') #output - 5 batik motifs 
 ])
+
+train_ds = keras.utils.image_dataset_from_directory (
+    'data/train_augmented',
+    validation_split=0.2,
+    subset="both",
+    image_size=(224, 224),
+    batch_size=32,
+    label_mode='int'
+)
+
+test_ds = keras.utils.image_dataset_from_directory(
+    'data/test_processed',
+    image_size=(224,224),
+    batch_size=32,
+    label_mode='int',
+    shuffle=False
+)
+
+model.compile(optimizer=keras.optimizers.Adam(), 
+              loss = keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics =[keras.metrics.BinaryAccuracy()])
+
+model.fit(train_ds, epochs= 20)
