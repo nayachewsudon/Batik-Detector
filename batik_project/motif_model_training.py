d053889a13881a0f46ee5ab77b74772133779d59
Kanaya Ozora <nayachewsudon@users.noreply.github.com>
2026-01-23 11:54:45 +0100

    Added f1-score for evaluation and early-stopping to prevent overfitting

--- batik_project/motif_model_training.py
+++ batik_project/motif_model_training.py
@@ -7,6 +7,9 @@ from tensorflow.keras import layers, models
 from tensorflow.keras import ops
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import classification_report, confusion_matrix
+from tensorflow.keras.callbacks import EarlyStopping
+import numpy as np
 
 base_model = MobileNetV2(
     weights = 'imagenet',
@@ -31,6 +34,14 @@ def apply_preprocessing(images, labels):
 
     return preprocess_input(images), labels
 
+#To stop model from overfitting
+early_stop = EarlyStopping(
+    monitor='val_loss',
+    patience= 3,
+    restore_best_weights=True
+)
+
+#Training Data
 train_ds = keras.utils.image_dataset_from_directory (
     'data/train_augmented',
     validation_split=0.2,
@@ -41,6 +52,7 @@ train_ds = keras.utils.image_dataset_from_directory (
     label_mode='int'
 ).map(apply_preprocessing)
 
+#Validation Data
 val_ds = keras.utils.image_dataset_from_directory (
     'data/train_augmented',
     validation_split=0.2,
@@ -51,6 +63,7 @@ val_ds = keras.utils.image_dataset_from_directory (
     label_mode='int'
 ).map(apply_preprocessing)
 
+#Testing Data
 test_ds = keras.utils.image_dataset_from_directory(
     'data/test_processed',
     image_size=(224,224),
@@ -66,17 +79,30 @@ model.compile(optimizer=keras.optimizers.Adam(),
 print("Model Summary:")
 model.summary()
 
+#Model Training
 print("Training starts NOW...")
 history = model.fit(
     train_ds,
     validation_data=val_ds, 
-    epochs = 20
+    epochs = 20,
+    callbacks=[early_stop]
 )
 
+#Test Evaluation
 print("Evaluate on test set NOW...")
 test_loss, test_acc = model.evaluate(test_ds)
 print(f"Test accuracy: {test_acc:.4f}")
 print(f"Test loss: {test_loss:.4f}")
 
+
+#Classification Report
+y_true = np.concatenate([y for x, y in test_ds], axis=0) #True class labels
+y_pred_probs = model.predict(test_ds) #Predicted
+y_pred = np.argmax(y_pred_probs, axis = 1)
+
+print("Classification Report: ")
+print(classification_report(y_true, y_pred, target_names=['Batik Kawung', 'Batik Megamendung', 'Batik Parang']))
+
+#Save Model
 model.save('batik_model.keras')
 print()
